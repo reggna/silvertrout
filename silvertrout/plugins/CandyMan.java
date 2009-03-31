a010b3e20950b9f7bf6cb242418b67f22ee7c0aa
Gustav Tiger <tiggex@gmail.com>
2009-03-31 19:00:13 +0000

    Adjusted CandyMan plugin to be more of plugin tutorial
    git-svn-id: https://silvertrout.googlecode.com/svn/trunk@168 1d2c35c4-c6bc-11dd-a2c4-6dc3ad32ce87

--- silvertrout/plugins/CandyMan.java
+++ silvertrout/plugins/CandyMan.java
@@ -25,25 +25,58 @@ import java.util.Random;
 import silvertrout.Channel;
 import silvertrout.User;
 
+/**
+ * CandyMan - Example plugin for Silvertrout IRC bot framework.
+ *
+ * This is a rather useless plugin but it demonstrates some vital parts of the
+ * framework so its at least a good example on how to do things. The plugin is
+ * still a work in progress.
+ *
+ * To create a plugin you create a class that extends silvertrout.Plugin and
+ * override (with @Override) the on-methods you want to implement. There are
+ * many methods like this. A few examples are onTick, onPrivmsg, onMode, onLoad
+ * and onQuit. Make sure you put your plugin in the silvertrout.plugins package
+ * too.
+ *
+ * If you plan to do computation or run external programs or something that may
+ * take more then a 10th of second or so you may want to use threads so your 
+ * plugin do not disrupt what the bot is otherwise doing.
+ *
+ * @see silvertrout.Plugin
+ */
 public class CandyMan extends silvertrout.Plugin {
 
     private Random r;
 
-    public CandyMan() {
+    /**
+     * This function gets called when loading the plugin. You could also use the
+     * constructor but if you instead use the onLoad function you get your 
+     * settings and you can be sure that the plugin is connected to a network
+     * - fetch with getNetwork().
+     *
+     * You can of course combine an onLoad function with a constructor. Just 
+     * make sure you know what is not initialized when using the constructor.
+     */
+    public void onLoad(Map<String, String> settings) {
         r = new Random();
     }
-
+    
     @Override
     public void onPrivmsg(User user, Channel channel, String message) {
-        // Only in channels:
+        // Only in channels - as an alternative here we could instead overload
+        // the onPrivmsg(User user, String message) that gets called when a user
+        // contacts you directly with a private message.
+        // 
+        // As a default that function calls this function but sets the channel
+        // parameter to null.
         if (channel != null) {
 
-            String[] icecreams = {"Daimstrut", "Magnum", "Top Hat", "Cornetto", "Solero",
-                "Nogger", "88:an", "Tip Top"};
+            // Our icecream list
+            String[] icecreams = {"Daimstrut", "Magnum", "Top Hat", "Cornetto", 
+                    "Solero", "Nogger", "88:an", "Tip Top"};
+            // Our candy list
             String[] candy = {"Toblerone", "Daimpåse", "Twix", "påse med Bilar",
-                "Kexchoklad", "stor påse med Chips"};
-            String[] drugs = {"haschbrownie", "joint", "lina kokain",
-                "spruta heroin", "lapp lsd"};
+                    "Kexchoklad", "stor påse med Chips"};
 
             String what = new String();
 
@@ -51,9 +84,6 @@ public class CandyMan extends silvertrout.Plugin {
                 what = icecreams[r.nextInt(icecreams.length)];
             } else if (message.equals("!godis")) {
                 what = candy[r.nextInt(candy.length)];
-                
-            } else if (message.equals("!knark")) {
-                what = drugs[r.nextInt(drugs.length)];
             } else {
                 return;
             }
