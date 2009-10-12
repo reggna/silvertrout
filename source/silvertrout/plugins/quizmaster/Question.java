694b4965ff4777bfd52a81fcacb54f5a20ae7b6c
Gustav Tiger <tiggex@gmail.com>
2009-10-12 16:47:22 +0000

    * Splited Quizmaster into a more files * Updated readme with note about config.xml
    git-svn-id: https://silvertrout.googlecode.com/svn/trunk@207 1d2c35c4-c6bc-11dd-a2c4-6dc3ad32ce87

---
+++ source/silvertrout/plugins/quizmaster/Question.java
@@ -0,0 +1,63 @@
+/*   _______ __ __                    _______                    __   
+ *  |     __|__|  |.--.--.-----.----.|_     _|.----.-----.--.--.|  |_ 
+ *  |__     |  |  ||  |  |  -__|   _|  |   |  |   _|  _  |  |  ||   _|
+ *  |_______|__|__| \___/|_____|__|    |___|  |__| |_____|_____||____|
+ * 
+ *  Copyright 2008 - Gustav Tiger, Henrik Steen and Gustav "Gussoh" Sohtell
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package silvertrout.plugins.quizmaster;
+
+import java.util.ArrayList;
+
+/**
+ * Question for the Quizmaster plugin.
+ *
+ */
+class Question {
+	// Location information (for report / debug)
+    String file         = null;
+	int    row          = -1;
+	// Category
+	String category     = null;
+    // Question and hint        
+    String questionLine = null;
+    String hintLine     = null;
+    // Max attempts 
+    int attempts        = 100;
+	// Required amount of answers
+    int required        = 1;
+    // Number of total hints
+    int hintCount       = 7;
+    
+	// Hint struct
+    class Hint {
+        String hint      = null;
+        int    scoredec  = 1; //?
+    }
+    // Hint collection
+	ArrayList<Hint>   hints   = new ArrayList<Hint>();
+	
+	// Answer struct
+    class Answer {
+        String  answer   = null;
+        int     score    = 5;
+        boolean required = false;
+    }
+	// Answer collection
+    ArrayList<Answer> answers = new ArrayList<Answer>();
+
+}
