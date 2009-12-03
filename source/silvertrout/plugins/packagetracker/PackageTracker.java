93fda9e9732fb7dffc68b6850597ffec2e553180
Gustav Tiger <tiggex@gmail.com>
2009-12-03 18:37:40 +0000

    Hopefully fixed a few packet errors
    git-svn-id: https://silvertrout.googlecode.com/svn/trunk@277 1d2c35c4-c6bc-11dd-a2c4-6dc3ad32ce87

--- source/silvertrout/plugins/packagetracker/PackageTracker.java
+++ source/silvertrout/plugins/packagetracker/PackageTracker.java
@@ -1,20 +1,20 @@
-/*   _______ __ __                    _______                    __   
- *  |     __|__|  |.--.--.-----.----.|_     _|.----.-----.--.--.|  |_ 
+/*   _______ __ __                    _______                    __
+ *  |     __|__|  |.--.--.-----.----.|_     _|.----.-----.--.--.|  |_
  *  |__     |  |  ||  |  |  -__|   _|  |   |  |   _|  _  |  |  ||   _|
  *  |_______|__|__| \___/|_____|__|    |___|  |__| |_____|_____||____|
- * 
+ *
  *  Copyright 2008 - Gustav Tiger, Henrik Steen and Gustav "Gussoh" Sohtell
- * 
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
@@ -49,7 +49,7 @@ import silvertrout.User;
  * there are any new events. These are printed to the channel and stored for
  * users wanting to check the last status (e.g if missed the anouncement).
  *
- * Supplies commands: !listpackages, !addpackage ID, !statuspackage ID 
+ * Supplies commands: !listpackages, !addpackage ID, !statuspackage ID
  *                    and !removepackage ID.
  *s
  */
@@ -60,25 +60,25 @@ public class PackageTracker extends silvertrout.Plugin {
         public String  id;
         public String  customer        = "Unknown customer";
         public String  service         = "Unknown service";
-         
+
         public String  recieverZipCode = "Unknown zip code";
         public String  recieverCity    = "Unknown city";
 
         public String  weight          = "?";
-        
+
         public String  dateSent        = "Unknown send date";
         public String  dateDelivered   = "Unknown deliver date";
-        
+
         public int     lastDate;
         public int     lastTime;
-        
+
         public final ArrayList<PackageEvent> events = new ArrayList<PackageEvent>();
-        
+
         public Channel channel;
-        
+
         @Override
         public String toString() {
-            return "Package (" + service + ", " + weight + " kg) " 
+            return "Package (" + service + ", " + weight + " kg) "
                     + id + " from " + customer + " on route to "
                     + recieverZipCode + " " + recieverCity;
         }
@@ -88,10 +88,10 @@ public class PackageTracker extends silvertrout.Plugin {
 
         public String description;
         public String location;
-        
+
         public int    date;
         public int    time;
-        
+
         @Override
         public String toString()
         {
@@ -126,14 +126,14 @@ public class PackageTracker extends silvertrout.Plugin {
         p.channel  = channel;
         p.lastDate = Integer.parseInt("19700101");
         p.lastTime = Integer.parseInt("0000");
-        
+
         update(p);
-        
+
         packages.add(p);
-        
+
         return true;
     }
-    
+
     public boolean remove(String id)
     {
         for(Package p: packages)
@@ -146,7 +146,7 @@ public class PackageTracker extends silvertrout.Plugin {
         }
         return false;
     }
-    
+
     public Package get(String id)
     {
         for(Package p: packages)
@@ -158,46 +158,47 @@ public class PackageTracker extends silvertrout.Plugin {
         }
         return null;
     }
-    
+
     public String format(int date, int time)
     {
         int year   = date / 10000;
         int month  = (date % 10000) / 100;
         int day    = date % 100;
-        
+
         int hour   = time / 100;
         int minute = time % 100;
-;        
+;
         return String.format("%1$04d-%2$02d-%3$02d %4$02d:%5$02d", year, month, day, hour, minute);
     }
-    
+
     public void update(Package p) {
-        
+
         ArrayList<PackageEvent> events = fetch(p);
-        
-        if(events.size() > 0) {        
+
+        if(events.size() > 0) {
 
             p.events.addAll(events);
             String chan = p.channel.getName();
-            
+
             getNetwork().getConnection().sendPrivmsg(chan, p.toString());
-                        
+
             for(PackageEvent event: events) {
                 getNetwork().getConnection().sendPrivmsg(chan, " * " + event);
-                
-                if(event.date >= p.lastDate && event.time > p.lastTime) {
+
+                if(event.date > p.lastDate || (event.date == p.lastDate
+                        && event.time > p.lastTime)) {
                     p.lastDate = event.date;
                     p.lastTime = event.time;
-                }            
+                }
             }
-        }        
+        }
 
     }
-    
+
     public ArrayList<PackageEvent> fetch(Package p) {
-    
-        ArrayList<PackageEvent> events = new ArrayList<PackageEvent>();   
-    
+
+        ArrayList<PackageEvent> events = new ArrayList<PackageEvent>();
+
         // Connect and fetch package information:
         try {
 
@@ -210,8 +211,8 @@ public class PackageTracker extends silvertrout.Plugin {
             Document doc = builder.parse(con.getInputStream());
 
             // XXX, DEBUG!!
-            if(p.id.equals("1000"))
-              doc = builder.parse(new java.io.FileInputStream("/home/tigge/Desktop/packet.xml"));
+            //if(p.id.equals("1000"))
+            //  doc = builder.parse(new java.io.FileInputStream("/home/tigge/Desktop/packet.xml"));
 
             // Update basic information:
             //if(doc.getElementsByTagName("parcel").getLength() > 0)
@@ -230,7 +231,7 @@ public class PackageTracker extends silvertrout.Plugin {
                 p.dateDelivered   = doc.getElementsByTagName("datedelivered").item(0).getTextContent();
             if(doc.getElementsByTagName("actualweight").getLength() > 0)
                 p.weight          = doc.getElementsByTagName("actualweight").item(0).getTextContent();
-            
+
             NodeList eventList = doc.getElementsByTagName("event");
             System.out.println("Got " + eventList.getLength() + " events");
             for(int i = 0; i < eventList.getLength(); i++) {
@@ -247,19 +248,19 @@ public class PackageTracker extends silvertrout.Plugin {
                     } else if(n.getNodeName().equals("description")) {
                         pe.description = n.getTextContent();
                     }
-                }    
+                }
                 if(pe.date >= p.lastDate && pe.time > p.lastTime) {
                     events.add(pe);
-                }            
+                }
             }
 
-  
+
         } catch (Exception e) {
             System.out.println("Failed to update package " + p.id);
             e.printStackTrace();
             return new ArrayList<PackageEvent>();
         }
-        
+
         return events;
     }
 
@@ -285,26 +286,26 @@ public class PackageTracker extends silvertrout.Plugin {
         } else if (parts.length == 2 && command.equals("!addpackage")) {
             if (add(parts[1], to)) {
                 Package p = packages.get(packages.size() - 1);
-                getNetwork().getConnection().sendPrivmsg(to.getName(), 
+                getNetwork().getConnection().sendPrivmsg(to.getName(),
                         "Added: " + p);
             } else {
-                getNetwork().getConnection().sendPrivmsg(to.getName(), 
+                getNetwork().getConnection().sendPrivmsg(to.getName(),
                         "Failed to add package");
             }
         // Remove package:
         } else if (command.equals("!removepackage")) {
             if (remove(parts[1])) {
-                getNetwork().getConnection().sendPrivmsg(to.getName(), 
+                getNetwork().getConnection().sendPrivmsg(to.getName(),
                         "Removed: Package (" + parts[1] + ")");
             } else {
-                getNetwork().getConnection().sendPrivmsg(to.getName(), 
+                getNetwork().getConnection().sendPrivmsg(to.getName(),
                         "Failed to remove package");
             }
         // Status on package:
         } else if (command.equals("!statuspackage")) {
             Package p = get(parts[1]);
             if(p != null) {
-                getNetwork().getConnection().sendPrivmsg(to.getName(), 
+                getNetwork().getConnection().sendPrivmsg(to.getName(),
                         p.toString());
                 if(p.events.size() > 0) {
                     getNetwork().getConnection().sendPrivmsg(to.getName(),
@@ -314,12 +315,12 @@ public class PackageTracker extends silvertrout.Plugin {
                         " * No events for this package");
                 }
             } else {
-                getNetwork().getConnection().sendPrivmsg(to.getName(), 
+                getNetwork().getConnection().sendPrivmsg(to.getName(),
                         "Could not find package");
             }
-            
+
         }
-        
+
 
     }
 
